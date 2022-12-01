fbda30220ef2b76929042bc21ec07e7538a8187b
Devin Robison <drobison@nvidia.com>
2022-12-01 14:54:07 -0700

    Start Morpheus transfer

--- cmake/morpheus_utils/environment_config/package_manager/register_api.cmake
+++ cmake/morpheus_utils/environment_config/package_manager/register_api.cmake
@@ -18,6 +18,72 @@ include_guard(GLOBAL)
 # Capture the directory where the function is defined
 set(MORPHEUS_UTILS_ENVCFG_PM_DIR "${CMAKE_CURRENT_LIST_DIR}")
 
+function(morpheus_utils_initialize_package_manager_2
+    USE_CONDA_VAR_NAME
+    BUILD_SHARED_LIBS_VAR_NAME
+    VCPKG_TOOLCHAIN_VAR_NAME
+    VCPKG_DEFAULT_BINARY_CACHE_VAR_NAME
+    )
+
+  set(USE_CONDA ${${USE_CONDA_VAR_NAME}})
+  set(VCPKG_TOOLCHAIN ${${VCPKG_TOOLCHAIN_VAR_NAME}})
+  set(VCPKG_DEFAULT_BINARY_CACHE ${${VCPKG_DEFAULT_BINARY_CACHE}})
+
+  if(DEFINED CMAKE_TOOLCHAIN_FILE)
+    message(STATUS "[CMAKE_TOOLCHAIN_FILE] is defined (${CMAKE_TOOLCHAIN_FILE}), using custom toolchain")
+    message(STATUS "Conda and VCPKG environment variables will be ignored.")
+  elseif (USE_CONDA AND DEFINED ENV{CONDA_PREFIX})
+    message(STATUS "${USE_CONDA_VAR_NAME} is defined and CONDA environment ($ENV{CONDA_PREFIX}) exists.")
+    message(STATUS "VCPKG environment variables will be ignored.")
+    rapids_cmake_support_conda_env(conda_env MODIFY_PREFIX_PATH)
+
+    if (CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT AND DEFINED ENV{CONDA_PREFIX})
+      message(STATUS "No CMAKE_INSTALL_PREFIX argument detected, setting to: $ENV{CONDA_PREFIX}")
+      set(CMAKE_INSTALL_PREFIX "$ENV{CONDA_PREFIX}" CACHE STRING "" FORCE)
+    endif()
+
+    message(STATUS "Prepending CONDA_PREFIX ($ENV{CONDA_PREFIX}) to CMAKE_FIND_ROOT_PATH")
+    list(PREPEND CMAKE_FIND_ROOT_PATH "$ENV{CONDA_PREFIX}")
+    list(REMOVE_DUPLICATES CMAKE_FIND_ROOT_PATH)
+
+  elseif(DEFINED ENV{VCPKG_ROOT})
+    if(NOT EXISTS "$ENV{VCPKG_ROOT}")
+      message(FATAL_ERROR "Vcpkg env 'VCPKG_ROOT' set to '$ENV{VCPKG_ROOT}' but file does not exist! Exiting...")
+      return()
+    endif()
+
+    message(STATUS "VCPKG_ROOT is defined and root directory ($ENV{VCPKG_ROOT}) exists.")
+    message(STATUS "VCPKG will be used for morpheus build")
+
+    set(CMAKE_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" CACHE STRING "")
+    set(USING_VCPKG True PARENT_SCOPE)
+
+    message(STATUS "Creating project. If this hangs, check 'VCPKG_ROOT' environment variable.
+    Should not take more than a few seconds to see additional output")
+
+    # If using shared libs (the default) use a custom triplet file to use dynamic linking
+    if(${BUILD_SHARED_LIBS_VAR_NAME})
+      set(VCPKG_OVERLAY_TRIPLETS "${CMAKE_CURRENT_SOURCE_DIR}/cmake/vcpkg_triplets")
+      set(VCPKG_TARGET_TRIPLET "x64-linux-dynamic")
+    endif()
+
+    # Once the build type is set, remove any dumb vcpkg debug folders from the
+    # search paths. Without this FindBoost fails since it defaults to the debug
+    # binaries
+    if(DEFINED CMAKE_BUILD_TYPE AND NOT CMAKE_BUILD_TYPE MATCHES "^[Dd][Ee][Bb][Uu][Gg]$")
+      message(STATUS "Release Build: Removing debug paths from CMAKE_PREFIX_PATH and CMAKE_FIND_ROOT_PATH")
+      list(REMOVE_ITEM CMAKE_PREFIX_PATH "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug")
+      list(REMOVE_ITEM CMAKE_FIND_ROOT_PATH "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug")
+    endif()
+
+    # Help vcpkg out on CI systems by ensuring the cache directory exists
+    if(DEFINED ENV{VCPKG_DEFAULT_BINARY_CACHE} AND NOT EXISTS "$ENV{VCPKG_DEFAULT_BINARY_CACHE}")
+      message(STATUS "VCPKG binary cache missing. Creating directory. Cache location: $ENV{VCPKG_DEFAULT_BINARY_CACHE}")
+      file(MAKE_DIRECTORY "$ENV{VCPKG_DEFAULT_BINARY_CACHE}")
+    endif()
+  endif()
+endfunction()
+
 function(morpheus_utils_initialize_package_manager
     USE_CONDA_VAR_NAME
     VCPKG_TOOLCHAIN_VAR_NAME
@@ -32,7 +98,9 @@ function(morpheus_utils_initialize_package_manager
   if(USE_CONDA)
     # Using conda path. Check for conda environment
     if(NOT DEFINED ENV{CONDA_PREFIX})
-      message(WARNING "Option 'MORPHEUS_UTILS_USE_CONDA' is set to ON but no conda environment detected! Ensure you have called `conda activate` before configuring. Third party dependencies are likely to not be found.")
+      message(WARNING "Option 'MORPHEUS_UTILS_USE_CONDA' is set to ON but no conda environment
+        detected! Ensure you have called `conda activate` before configuring.
+        Third party dependencies are likely to not be found.")
     else()
       message(STATUS "Conda environment detected at '$ENV{CONDA_PREFIX}'. Skipping Vcpkg")
     endif()
--- cmake/morpheus_utils/environment_config/register_api.cmake
+++ cmake/morpheus_utils/environment_config/register_api.cmake
@@ -22,7 +22,7 @@ list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
 include(ccache/register_api)
 include(code_coverage/register_api)
 include(iwyu/register_api)
-include(package_manager/register_api)
+#include(package_manager/register_api)
 include(rapids_cmake/register_api)
 
 list(POP_BACK CMAKE_MODULE_PATH)
