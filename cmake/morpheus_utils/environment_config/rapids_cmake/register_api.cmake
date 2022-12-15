6c65b6a78cadd8972d773c5eb715af5db7f560fc
Devin Robison <drobison@nvidia.com>
2022-12-15 16:12:42 -0700

    Reorg rapids cpm init calls

--- cmake/morpheus_utils/environment_config/rapids_cmake/register_api.cmake
+++ cmake/morpheus_utils/environment_config/rapids_cmake/register_api.cmake
@@ -37,34 +37,11 @@ macro(morpheus_utils_ensure_rapids_cpm_init)
     set(rapids_cmake_version "22.10")
   endif()
 
-  if (NOT MORPHEUS_UTILS_RAPIDS_CPM_INITIALIZED)
-    morpheus_utils_initialize_rapids_cmake(${rapids_cmake_version})
-    set(MORPHEUS_UTILS_RAPIDS_CPM_INITIALIZED TRUE CACHE STRING "")
-    mark_as_advanced(MORPHEUS_UTILS_RAPIDS_CPM_INITIALIZED)
-  endif()
+  morpheus_utils_initialize_rapids_cmake(rapids_cmake_version)
 endmacro()
 
 function(morpheus_utils_initialize_rapids_cmake RAPIDS_VERSION_VAR_NAME)
-  set(RAPIDS_CMAKE_VERSION "${${RAPIDS_VERSION_VAR_NAME}}" CACHE STRING "Version of rapids-cmake to use")
-
-  # Download and load the repo according to the rapids-cmake instructions if it does not exist
-  if(NOT EXISTS ${CMAKE_BINARY_DIR}/RAPIDS_CMAKE.cmake)
-    message(STATUS "Downloading RAPIDS CMake Version: ${RAPIDS_CMAKE_VERSION}")
-    file(
-      DOWNLOAD https://raw.githubusercontent.com/rapidsai/rapids-cmake/branch-${RAPIDS_CMAKE_VERSION}/RAPIDS.cmake
-      ${CMAKE_BINARY_DIR}/RAPIDS_CMAKE.cmake
-    )
-  endif()
-
-  # Now load the file
-  include(${CMAKE_BINARY_DIR}/RAPIDS_CMAKE.cmake)
-
-  # Load Rapids Cmake packages
-  include(rapids-cmake)
-  include(rapids-cpm)
-  include(rapids-cuda)
-  include(rapids-export)
-  include(rapids-find)
+  include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/ensure_rapids_cmake_init.cmake")
 endfunction()
 
 macro(morpheus_utils_initialize_cuda_arch project_name)
