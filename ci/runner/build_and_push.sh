928c3e4f1744952e76791b9520f84ecf707a36cd
Michael Demoret <youmdemoret@nvidia.com>
2023-01-25 21:42:26 +0000

    Fixing some issues after moving the ci/runner scripts to utilities

--- ci/runner/build_and_push.sh
+++ ci/runner/build_and_push.sh
@@ -14,11 +14,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Get the repo name from git. Either Morpheus or MRC
+REPO_NAME=$(basename -s .git `git config --get remote.origin.url`)
+
+# Convert it to lower
+REPO_NAME=$(echo "${REPO_NAME}" | tr '[:upper:]' '[:lower:]')
+
 DOCKER_TARGET=${DOCKER_TARGET:-"base" "driver"}
 DOCKER_BUILDKIT=${DOCKER_BUILDKIT:-1}
 DOCKER_REGISTRY_SERVER=${DOCKER_REGISTRY_SERVER:-"nvcr.io"}
 DOCKER_REGISTRY_PATH=${DOCKER_REGISTRY_PATH:-"/ea-nvidia-morpheus/morpheus"}
-DOCKER_TAG_PREFIX=${DOCKER_TAG_PREFIX:-"mrc-ci"}
+DOCKER_TAG_PREFIX=${DOCKER_TAG_PREFIX:-"${REPO_NAME}-ci"}
 DOCKER_TAG_POSTFIX=${DOCKER_TAG_POSTFIX:-"$(date +'%y%m%d')"}
 DOCKER_EXTRA_ARGS=${DOCKER_EXTRA_ARGS:-""}
 
@@ -26,6 +32,9 @@ SKIP_BUILD=${SKIP_BUILD:-""}
 SKIP_PUSH=${SKIP_PUSH:-""}
 
 set -e
+
+# Get the runner context from the user supplied argument or default to ci/runner
+RUNNER_CONTEXT=${1:-./ci/runner}
 
 function get_image_full_name() {
    echo "${DOCKER_REGISTRY_SERVER}${DOCKER_REGISTRY_PATH}:${DOCKER_TAG_PREFIX}-${build_target}-${DOCKER_TAG_POSTFIX}"
@@ -35,7 +44,7 @@ if [[ "${SKIP_BUILD}" == "" ]]; then
     for build_target in ${DOCKER_TARGET[@]}; do
         FULL_NAME=$(get_image_full_name)
         echo "Building target \"${build_target}\" as ${FULL_NAME}";
-        docker buildx build --network=host ${DOCKER_EXTRA_ARGS} --target ${build_target} -t ${FULL_NAME} -f ./Dockerfile .
+        docker buildx build --network=host ${DOCKER_EXTRA_ARGS} --target ${build_target} -t ${FULL_NAME} -f ${RUNNER_CONTEXT}/Dockerfile .
     done
 fi
 
