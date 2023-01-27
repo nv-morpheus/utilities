6ccdf287f7e385ec804cd9659c0f403ec6d485f2
Michael Demoret <mdemoret@nvidia.com>
2023-01-27 13:58:28 -0700

    Fixes incorrect URL for MRC repo

--- cmake/morpheus_utils/package_config/mrc/Configure_mrc.cmake
+++ cmake/morpheus_utils/package_config/mrc/Configure_mrc.cmake
@@ -32,9 +32,9 @@ function(morpheus_utils_configure_mrc)
     INSTALL_EXPORT_SET
       ${PROJECT_NAME}-exports
     CPM_ARGS
-      GIT_REPOSITORY  https://github.com/nv-morpheus/utilities.git
-      GIT_TAG         v${MRC_VERSION}
-      GIT_SHALLOW     TRUE
+      GIT_REPOSITORY  https://github.com/nv-morpheus/MRC.git
+      GIT_TAG         branch-${MRC_VERSION}
+      GIT_SHALLOW     FALSE
       OPTIONS         "MRC_BUILD_EXAMPLES OFF"
                       "MRC_BUILD_TESTS OFF"
                       "MRC_BUILD_BENCHMARKS OFF"
