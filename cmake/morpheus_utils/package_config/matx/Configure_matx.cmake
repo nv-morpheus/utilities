735d6ec2b01941ba8c2ea40d174d3ec44844ef88
David Gardner <dagardner@nvidia.com>
2023-05-10 13:07:24 -0700

    Revert "Revert "Adopt MatX v0.4.0 (#36)""
    This reverts commit 105a1c9af77e24921eb314e5bbde05baf1f4c0b1.

--- cmake/morpheus_utils/package_config/matx/Configure_matx.cmake
+++ cmake/morpheus_utils/package_config/matx/Configure_matx.cmake
@@ -22,8 +22,8 @@ function(morpheus_utils_configure_matx)
   list(APPEND CMAKE_MESSAGE_CONTEXT "matx")
 
   morpheus_utils_assert_cpm_initialized()
-  set(MATX_VERSION "0.3.0" CACHE STRING "Version of MatX to use")
-  set(MATX_TAG "015908f" CACHE STRING "Tag of MatX to use")
+  set(MATX_VERSION "0.4.0" CACHE STRING "Version of MatX to use")
+  set(MATX_TAG "v${MATX_VERSION}" CACHE STRING "Tag of MatX to use")
 
   if(CUDAToolkit_FOUND AND (CUDAToolkit_VERSION VERSION_GREATER "11.5"))
 
@@ -38,6 +38,7 @@ function(morpheus_utils_configure_matx)
       CPM_ARGS
         GIT_REPOSITORY  https://github.com/NVIDIA/MatX.git
         GIT_TAG         "${MATX_TAG}"
+        GIT_SHALLOW     TRUE
         OPTIONS         "MATX_BUILD_32_BIT ON"
                         "MATX_BUILD_BENCHMARKS OFF"
                         "MATX_BUILD_DOCS OFF"
