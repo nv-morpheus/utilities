4c9c9f89ee9f971165ca559ea16dbc6706ac0525
David Gardner <dagardner@nvidia.com>
2023-06-12 11:40:44 -0700

    wip

--- cmake/morpheus_utils/package_config/pybind11/Configure_pybind11.cmake
+++ cmake/morpheus_utils/package_config/pybind11/Configure_pybind11.cmake
@@ -24,19 +24,27 @@ function(morpheus_utils_configure_pybind11)
   set(PYBIND11_VERSION "2.10.4" CACHE STRING "Version of Pybind11 to use")
 
   # Needs a patch to change the internal tracker to use fiber specific storage instead of TSS
-  rapids_cpm_find(pybind11 ${PYBIND11_VERSION}
+  # rapids_cpm_find(pybind11 ${PYBIND11_VERSION}
+  # GLOBAL_TARGETS
+  # pybind11 pybind11::pybind11
+  # BUILD_EXPORT_SET
+  # ${PROJECT_NAME}-core-exports
+  # INSTALL_EXPORT_SET
+  # ${PROJECT_NAME}-core-exports
+  # CPM_ARGS
+  # GIT_REPOSITORY https://github.com/pybind/pybind11.git
+  # GIT_TAG "v${PYBIND11_VERSION}"
+  # GIT_SHALLOW TRUE
+  # OPTIONS "PYBIND11_INSTALL ON"
+  # "PYBIND11_TEST OFF"
+  # )
+  rapids_find_package(pybind11 ${PYBIND11_VERSION} REQUIRED
     GLOBAL_TARGETS
     pybind11 pybind11::pybind11
     BUILD_EXPORT_SET
-    ${PROJECT_NAME}-core-exports
+    ${PROJECT_NAME}-exports
     INSTALL_EXPORT_SET
-    ${PROJECT_NAME}-core-exports
-    CPM_ARGS
-    GIT_REPOSITORY https://github.com/pybind/pybind11.git
-    GIT_TAG "v${PYBIND11_VERSION}"
-    GIT_SHALLOW TRUE
-    OPTIONS "PYBIND11_INSTALL ON"
-    "PYBIND11_TEST OFF"
+    ${PROJECT_NAME}-exports
   )
 
   list(POP_BACK CMAKE_MESSAGE_CONTEXT)
