b6bc761bfebcb0769bae1a8791c4938c01e04329
Michael Demoret <mdemoret@nvidia.com>
2023-07-26 20:28:06 -0600

    Improving setup scripts between MRC and Morpheus

--- cmake/morpheus_utils/environment_config/rapids_cmake/register_api.cmake
+++ cmake/morpheus_utils/environment_config/rapids_cmake/register_api.cmake
@@ -30,6 +30,52 @@ macro(morpheus_utils_initialize_cuda_arch project_name)
   list(POP_BACK CMAKE_MESSAGE_CONTEXT)
 endmacro()
 
+macro(morpheus_utils_initialize_cuda)
+  list(APPEND CMAKE_MESSAGE_CONTEXT "cuda")
+
+  if(NOT DEFINED CMAKE_CUDA_ARCHITECTURES)
+    set(CMAKE_CUDA_ARCHITECTURES "native")
+    message(STATUS "CMAKE_CUDA_ARCHITECTURES was not defined. Defaulting to '${CMAKE_CUDA_ARCHITECTURES}' to build only for local architecture. Specify -DCMAKE_CUDA_ARCHITECTURES='ALL' to build for all archs.")
+  endif()
+
+  # Initialize CUDA architectures
+  rapids_cuda_init_architectures(${PROJECT_NAME})
+
+  # Delay enabling CUDA until after we have determined our CXX compiler
+  if(NOT DEFINED CMAKE_CUDA_HOST_COMPILER)
+  message(STATUS "Setting CUDA host compiler to match CXX compiler: ${CMAKE_CXX_COMPILER}")
+
+  # Only set the host compiler if we arent using clang. Using clang > 8ish is
+  # incompatible with CUDA 11.4/11.5/11.6. See Issue #102
+  if(NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+    set(CMAKE_CUDA_HOST_COMPILER ${CMAKE_CXX_COMPILER})
+  endif()
+  endif()
+
+  if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+  # Our version of NVCC officially only supports clang versions 3.2 - 13, we are now using 14
+  set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -allow-unsupported-compiler")
+
+  # Check if the major version of Clang is greater than 15
+  execute_process(COMMAND "${CMAKE_CXX_COMPILER}" "--version"
+      OUTPUT_VARIABLE clang_version_info
+      ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+  string(REGEX MATCH "version [0-9]+" clang_version_value ${clang_version_info})
+  string(REGEX REPLACE "version ([0-9]+)" "\\1" clang_version ${clang_version_value})
+
+  if(${clang_version} VERSION_LESS_EQUAL "15")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fcoroutines-ts")
+  endif()
+
+  endif()
+
+  # Now enable CUDA
+  enable_language(CUDA)
+
+  list(POP_BACK CMAKE_MESSAGE_CONTEXT)
+endmacro()
+
 function(morpheus_utils_initialize_package_manager
     USE_CONDA_VAR_NAME
     BUILD_SHARED_LIBS_VAR_NAME
