b543ef6fd2d86252567b9468191b89a78fa8ff24
Christopher Harris <xixonia@gmail.com>
2023-09-11 16:10:43 +0000

    fix typo

--- cmake/morpheus_utils/package_config/matx/Configure_matx.cmake
+++ cmake/morpheus_utils/package_config/matx/Configure_matx.cmake
@@ -47,7 +47,7 @@ function(morpheus_utils_configure_matx)
     )
 
     # Explicitly set this compiletime definition because it is not implicitly set when building MRC
-    # and Morphues in the same conda environment For more details, see this github issue...
+    # and Morphues in the same conda environment For more details, see this github pr...
     # https://github.com/nv-morpheus/utilities/pull/55
     target_compile_definitions(matx INTERFACE __STDC_FORMAT_MACROS)
 
