87fd531b34515fd364b34beeeba7ab28c33d7f5f
David Gardner <dagardner@nvidia.com>
2023-12-28 12:52:28 -0800

    Separate the clang specific cuda configs from invoking rapids_cuda_init_architectures.

--- cmake/morpheus_utils/environment_config/rapids_cmake/register_api.cmake
+++ cmake/morpheus_utils/environment_config/rapids_cmake/register_api.cmake
@@ -30,44 +30,35 @@ macro(morpheus_utils_initialize_cuda_arch project_name)
   list(POP_BACK CMAKE_MESSAGE_CONTEXT)
 endmacro()
 
-macro(morpheus_utils_initialize_cuda)
+macro(morpheus_utils_enable_cuda)
   list(APPEND CMAKE_MESSAGE_CONTEXT "cuda")
-
-  if(NOT DEFINED CMAKE_CUDA_ARCHITECTURES)
-    set(CMAKE_CUDA_ARCHITECTURES "native")
-    message(STATUS "CMAKE_CUDA_ARCHITECTURES was not defined. Defaulting to '${CMAKE_CUDA_ARCHITECTURES}' to build only for local architecture. Specify -DCMAKE_CUDA_ARCHITECTURES='ALL' to build for all archs.")
-  endif()
-
-  # Initialize CUDA architectures
-  rapids_cuda_init_architectures(${PROJECT_NAME})
 
   # Delay enabling CUDA until after we have determined our CXX compiler
   if(NOT DEFINED CMAKE_CUDA_HOST_COMPILER)
-  message(STATUS "Setting CUDA host compiler to match CXX compiler: ${CMAKE_CXX_COMPILER}")
+    message(STATUS "Setting CUDA host compiler to match CXX compiler: ${CMAKE_CXX_COMPILER}")
 
-  # Only set the host compiler if we arent using clang. Using clang > 8ish is
-  # incompatible with CUDA 11.4/11.5/11.6. See Issue #102
-  if(NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-    set(CMAKE_CUDA_HOST_COMPILER ${CMAKE_CXX_COMPILER})
-  endif()
+    # Only set the host compiler if we arent using clang. Using clang > 8ish is
+    # incompatible with CUDA 11.4/11.5/11.6. See Issue #102
+    if(NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+      set(CMAKE_CUDA_HOST_COMPILER ${CMAKE_CXX_COMPILER})
+    endif()
   endif()
 
   if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-  # Our version of NVCC officially only supports clang versions 3.2 - 13, we are now using 14
-  set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -allow-unsupported-compiler")
+    # Our version of NVCC officially only supports clang versions 3.2 - 13, we are now using 14
+    set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -allow-unsupported-compiler")
 
-  # Check if the major version of Clang is greater than 15
-  execute_process(COMMAND "${CMAKE_CXX_COMPILER}" "--version"
+    # Check if the major version of Clang is greater than 15
+    execute_process(COMMAND "${CMAKE_CXX_COMPILER}" "--version"
       OUTPUT_VARIABLE clang_version_info
       ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-  string(REGEX MATCH "version [0-9]+" clang_version_value ${clang_version_info})
-  string(REGEX REPLACE "version ([0-9]+)" "\\1" clang_version ${clang_version_value})
+    string(REGEX MATCH "version [0-9]+" clang_version_value ${clang_version_info})
+    string(REGEX REPLACE "version ([0-9]+)" "\\1" clang_version ${clang_version_value})
 
-  if(${clang_version} VERSION_LESS_EQUAL "15")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fcoroutines-ts")
-  endif()
-
+    if(${clang_version} VERSION_LESS_EQUAL "15")
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fcoroutines-ts")
+    endif()
   endif()
 
   # Now enable CUDA
@@ -79,8 +70,7 @@ endmacro()
 function(morpheus_utils_initialize_package_manager
     USE_CONDA_VAR_NAME
     BUILD_SHARED_LIBS_VAR_NAME
-    )
-
+)
   # Ensure rapids CMake is setup
   include(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/ensure_rapids_cmake_init.cmake)
 
@@ -90,7 +80,7 @@ function(morpheus_utils_initialize_package_manager
     message(STATUS "${USE_CONDA_VAR_NAME} is defined and CONDA environment ($ENV{CONDA_PREFIX}) exists.")
     rapids_cmake_support_conda_env(conda_env MODIFY_PREFIX_PATH)
 
-    if (CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT AND DEFINED ENV{CONDA_PREFIX})
+    if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT AND DEFINED ENV{CONDA_PREFIX})
       message(STATUS "No CMAKE_INSTALL_PREFIX argument detected, setting to: $ENV{CONDA_PREFIX}")
       set(CMAKE_INSTALL_PREFIX "$ENV{CONDA_PREFIX}" CACHE STRING "" FORCE)
     endif()
