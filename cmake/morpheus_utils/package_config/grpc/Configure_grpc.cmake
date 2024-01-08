09ac8e15f9f2c3c9213683d8845b99b4f34dd5b7
David Gardner <dagardner@nvidia.com>
2024-01-08 10:45:13 -0800

    Add helper method for configuring grpc

---
+++ cmake/morpheus_utils/package_config/grpc/Configure_grpc.cmake
@@ -0,0 +1,34 @@
+# =============================================================================
+# SPDX-FileCopyrightText: Copyright (c) 2020-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+
+include_guard(GLOBAL)
+
+function(morpheus_utils_configure_grpc)
+  list(APPEND CMAKE_MESSAGE_CONTEXT "grpc")
+
+  morpheus_utils_assert_cpm_initialized()
+  rapids_find_package(gRPC REQUIRED
+    GLOBAL_TARGETS
+    gRPC::address_sorting gRPC::gpr gRPC::grpc gRPC::grpc_unsecure gRPC::grpc++ gRPC::grpc++_alts gRPC::grpc++_error_details gRPC::grpc++_reflection
+    gRPC::grpc++_unsecure gRPC::grpc_plugin_support gRPC::grpcpp_channelz gRPC::upb gRPC::grpc_cpp_plugin gRPC::grpc_csharp_plugin gRPC::grpc_node_plugin
+    gRPC::grpc_objective_c_plugin gRPC::grpc_php_plugin gRPC::grpc_python_plugin gRPC::grpc_ruby_plugin
+    BUILD_EXPORT_SET ${PROJECT_NAME}-exports
+    INSTALL_EXPORT_SET ${PROJECT_NAME}-exports
+  )
+
+  list(POP_BACK CMAKE_MESSAGE_CONTEXT)
+endfunction()
