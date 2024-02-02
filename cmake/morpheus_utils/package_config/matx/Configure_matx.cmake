fa5012ef061f5d6b4600095a68489e2283b3134a
Christopher Harris <xixonia@gmail.com>
2024-02-02 19:18:30 +0000

    update matx

--- cmake/morpheus_utils/package_config/matx/Configure_matx.cmake
+++ cmake/morpheus_utils/package_config/matx/Configure_matx.cmake
@@ -22,7 +22,7 @@ function(morpheus_utils_configure_matx)
 
   morpheus_utils_assert_cpm_initialized()
   set(MATX_VERSION "0.7.0" CACHE STRING "Version of MatX to use")
-  set(MATX_TAG "d9b045b7269523d048001e99072c511707b0d169" CACHE STRING "Tag of MatX to use")
+  set(MATX_TAG "13076b0c392add7100f55e2aa651653a29f26bf4" CACHE STRING "Tag of MatX to use")
 
   if(CUDAToolkit_FOUND AND(CUDAToolkit_VERSION VERSION_GREATER "11.5"))
     # Build MatX with 32 bit indexes, this allows matx size types to match those of cuDF
