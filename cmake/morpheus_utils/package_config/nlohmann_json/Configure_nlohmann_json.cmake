bc834f5f362a7a1afa9f4cf8a23c699bc22c537b
Christopher Harris <xixonia@gmail.com>
2024-08-30 20:21:18 +0000

    add nlohmann_json

---
+++ cmake/morpheus_utils/package_config/nlohmann_json/Configure_nlohmann_json.cmake
@@ -0,0 +1,42 @@
+#=============================================================================
+# SPDX-FileCopyrightText: Copyright (c) 2020-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#=============================================================================
+
+include_guard(GLOBAL)
+
+function(morpheus_utils_configure_cccl)
+  list(APPEND CMAKE_MESSAGE_CONTEXT "cccl")
+
+  morpheus_utils_assert_cpm_initialized()
+  set(nlohmann_json_VERSION "3.11.3" CACHE STRING "Version of nlohmann_json to use")
+
+  include("${rapids-cmake-dir}/cpm/cccl.cmake")
+
+  rapids_cpm_find(
+    GLOBAL_TARGETS
+        hwloc hwloc::hwloc
+    BUILD_EXPORT_SET
+      ${PROJECT_NAME}-exports
+    INSTALL_EXPORT_SET
+      ${PROJECT_NAME}-exports
+    CPM_ARGS
+        GIT_REPOSITORY          https://github.com/nlohmann/json.git
+        GIT_TAG                 "v${nlohmann_json_VERSION}"
+        DOWNLOAD_ONLY           TRUE
+  )
+
+  list(POP_BACK CMAKE_MESSAGE_CONTEXT)
+endfunction()
