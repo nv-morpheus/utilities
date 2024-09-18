ce8410887ad229e9c84d216985ada77eda263b49
Christopher Harris <xixonia@gmail.com>
2024-09-18 14:45:16 -0500

    Branch 24.06 RAPIDS 24.06 (#77)
    * updates for rapids-24.06
    
    * remove unused comments
    
    * remove unecessary patch header

--- cmake/morpheus_utils/package_config/matx/Configure_matx.cmake
+++ cmake/morpheus_utils/package_config/matx/Configure_matx.cmake
@@ -21,8 +21,7 @@ function(morpheus_utils_configure_matx)
   list(APPEND CMAKE_MESSAGE_CONTEXT "matx")
 
   morpheus_utils_assert_cpm_initialized()
-  set(MATX_VERSION "0.7.0" CACHE STRING "Version of MatX to use")
-  set(MATX_TAG "13076b0c392add7100f55e2aa651653a29f26bf4" CACHE STRING "Tag of MatX to use")
+  set(MATX_VERSION "0.8.0" CACHE STRING "Version of MatX to use")
 
   if(CUDAToolkit_FOUND AND(CUDAToolkit_VERSION VERSION_GREATER "11.5"))
     # Build MatX with 32 bit indexes, this allows matx size types to match those of cuDF
@@ -36,7 +35,7 @@ function(morpheus_utils_configure_matx)
       CPM_ARGS
         PATCH_COMMAND git checkout -- . && git apply --whitespace=fix ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/patches/matx_cccl_interface_fix.patch
         GIT_REPOSITORY https://github.com/NVIDIA/MatX.git
-        GIT_TAG "${MATX_TAG}"
+        GIT_TAG "v${MATX_VERSION}"
         GIT_SHALLOW True
         OPTIONS "MATX_BUILD_32_BIT ON"
                 "MATX_BUILD_BENCHMARKS OFF"
