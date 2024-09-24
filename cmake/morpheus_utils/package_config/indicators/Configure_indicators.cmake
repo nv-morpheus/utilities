c9fe432e85f8601115ba9ddbde1cc3378f420bca
Yuchen Zhang <yuchenz@nvidia.com>
2024-09-24 08:46:20 -0700

    draft pr for dependency

---
+++ cmake/morpheus_utils/package_config/indicators/Configure_indicators.cmake
@@ -0,0 +1,25 @@
+include_guard(GLOBAL)
+
+function(morpheus_utils_configure_indicators)
+  list(APPEND CMAKE_MESSAGE_CONTEXT "indicators")
+
+  morpheus_utils_assert_cpm_initialized()
+  set(INDICATORS_VERSION "2.0" CACHE STRING "Version of indicators to use")
+
+  rapids_cpm_find(indicators ${INDICATORS_VERSION}
+    GLOBAL_TARGETS
+      indicators indicators::indicators
+    BUILD_EXPORT_SET
+      ${PROJECT_NAME}-exports
+    INSTALL_EXPORT_SET
+      ${PROJECT_NAME}-exports
+    CPM_ARGS
+      GIT_REPOSITORY https://github.com/p-ranav/indicators.git
+      GIT_TAG "v${INDICATORS_VERSION}"
+      GIT_SHALLOW TRUE
+      OPTIONS "INDICATORS_INSTALL ON"
+              "INDICATORS_TEST OFF"
+  )
+
+  list(POP_BACK CMAKE_MESSAGE_CONTEXT)
+endfunction()
