d5638dc6200949aa6d5851a3626484c5eaa550e4
David Gardner <dagardner@nvidia.com>
2025-01-17 15:27:38 -0800

    Revert temporary changes

--- cmake/morpheus_utils/package_config/mrc/Configure_mrc.cmake
+++ cmake/morpheus_utils/package_config/mrc/Configure_mrc.cmake
@@ -1,18 +1,19 @@
-# =============================================================================
+#=============================================================================
 # Copyright (c) 2020-2022, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# =============================================================================
+#=============================================================================
+
 
 include_guard(GLOBAL)
 
@@ -35,21 +36,21 @@ function(morpheus_utils_configure_mrc)
     INSTALL_EXPORT_SET
       ${PROJECT_NAME}-exports
     CPM_ARGS
-      GIT_REPOSITORY git@github.com:dagardner-nv/MRC.git # temp using my fork do not merge
-      GIT_TAG david-25.02-x-25.02
-      GIT_SHALLOW FALSE
-      OPTIONS "MRC_BUILD_EXAMPLES OFF"
-    "MRC_BUILD_TESTS OFF"
-    "MRC_BUILD_BENCHMARKS OFF"
-    "MRC_BUILD_PYTHON ON"
-    "MRC_ENABLE_XTENSOR ON"
-    "MRC_ENABLE_MATX ON"
-    "MRC_USE_CONDA ${MORPHEUS_USE_CONDA}"
-    "MRC_USE_CCACHE ${MORPHEUS_USE_CCACHE}"
-    "MRC_USE_CLANG_TIDY ${MORPHEUS_USE_CLANG_TIDY}"
-    "MRC_PYTHON_INPLACE_BUILD OFF"
-    "MRC_PYTHON_PERFORM_INSTALL ON"
-    "MRC_PYTHON_BUILD_STUBS ${${PROJECT_NAME_UPPER}_PYTHON_BUILD_STUBS}"
+      GIT_REPOSITORY  https://github.com/nv-morpheus/MRC.git
+      GIT_TAG         branch-${MRC_VERSION}
+      GIT_SHALLOW     FALSE
+      OPTIONS         "MRC_BUILD_EXAMPLES OFF"
+                      "MRC_BUILD_TESTS OFF"
+                      "MRC_BUILD_BENCHMARKS OFF"
+                      "MRC_BUILD_PYTHON ON"
+                      "MRC_ENABLE_XTENSOR ON"
+                      "MRC_ENABLE_MATX ON"
+                      "MRC_USE_CONDA ${MORPHEUS_USE_CONDA}"
+                      "MRC_USE_CCACHE ${MORPHEUS_USE_CCACHE}"
+                      "MRC_USE_CLANG_TIDY ${MORPHEUS_USE_CLANG_TIDY}"
+                      "MRC_PYTHON_INPLACE_BUILD OFF"
+                      "MRC_PYTHON_PERFORM_INSTALL ON"
+                      "MRC_PYTHON_BUILD_STUBS ${${PROJECT_NAME_UPPER}_PYTHON_BUILD_STUBS}"
   )
 
   list(POP_BACK CMAKE_MESSAGE_CONTEXT)
