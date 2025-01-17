b5bde28ea3ea59cb772c456e82edb9dad64ee292
David Gardner <dagardner@nvidia.com>
2025-01-17 15:26:53 -0800

    Cleanup formatting

--- cmake/morpheus_utils/package_config/cccl/Configure_cccl.cmake
+++ cmake/morpheus_utils/package_config/cccl/Configure_cccl.cmake
@@ -1,4 +1,4 @@
-# =============================================================================
+#=============================================================================
 # SPDX-FileCopyrightText: Copyright (c) 2020-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -6,14 +6,14 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# =============================================================================
+#=============================================================================
 
 include_guard(GLOBAL)
 
@@ -35,6 +35,7 @@ function(morpheus_utils_configure_cccl)
       ${PROJECT_NAME}-exports
     INSTALL_EXPORT_SET
       ${PROJECT_NAME}-exports
+      # Set to match changes in https://github.com/rapidsai/rapids-cmake/pull/710/files
       GIT_TAG "83028fcdcfa8de51ab359e61ce07198a6003a65e"
       GIT_SHALLOW FALSE
   )
