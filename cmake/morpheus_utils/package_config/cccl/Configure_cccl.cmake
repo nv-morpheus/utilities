fdb256c55891c52931ec4dfa317a974fcf96067d
David Gardner <dagardner@nvidia.com>
2025-01-17 11:47:22 -0800

    Track cccl 2.7 ref: https://github.com/rapidsai/build-planning/issues/115

--- cmake/morpheus_utils/package_config/cccl/Configure_cccl.cmake
+++ cmake/morpheus_utils/package_config/cccl/Configure_cccl.cmake
@@ -1,4 +1,4 @@
-#=============================================================================
+# =============================================================================
 # SPDX-FileCopyrightText: Copyright (c) 2020-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -6,14 +6,14 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#=============================================================================
+# =============================================================================
 
 include_guard(GLOBAL)
 
@@ -21,7 +21,7 @@ function(morpheus_utils_configure_cccl)
   list(APPEND CMAKE_MESSAGE_CONTEXT "cccl")
 
   morpheus_utils_assert_cpm_initialized()
-  set(cccl_VERSION "2.5.0" CACHE STRING "Version of cccl to use")
+  set(cccl_VERSION "2.7.0" CACHE STRING "Version of cccl to use")
 
   include("${rapids-cmake-dir}/cpm/cccl.cmake")
 
@@ -35,6 +35,8 @@ function(morpheus_utils_configure_cccl)
       ${PROJECT_NAME}-exports
     INSTALL_EXPORT_SET
       ${PROJECT_NAME}-exports
+      GIT_TAG "83028fcdcfa8de51ab359e61ce07198a6003a65e"
+      GIT_SHALLOW FALSE
   )
 
   list(POP_BACK CMAKE_MESSAGE_CONTEXT)
