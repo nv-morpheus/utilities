f7fffc5eff29d42cb56bfc4beb1a88955421cf68
David Gardner <dagardner@nvidia.com>
2025-02-11 13:15:51 -0800

    Update to pybind 2.12.1 to pick up Python 3.12 compatibility, temporarily disable the gil patch

--- cmake/morpheus_utils/package_config/pybind11/Configure_pybind11.cmake
+++ cmake/morpheus_utils/package_config/pybind11/Configure_pybind11.cmake
@@ -21,7 +21,7 @@ function(morpheus_utils_configure_pybind11)
   list(APPEND CMAKE_MESSAGE_CONTEXT "pybind11")
 
   morpheus_utils_assert_cpm_initialized()
-  set(PYBIND11_VERSION "2.8.1" CACHE STRING "Version of Pybind11 to use")
+  set(PYBIND11_VERSION "2.12.1" CACHE STRING "Version of Pybind11 to use")
 
   # Needs a patch to change the internal tracker to use fiber specific storage instead of TSS
   rapids_cpm_find(pybind11 ${PYBIND11_VERSION}
@@ -35,7 +35,7 @@ function(morpheus_utils_configure_pybind11)
       GIT_REPOSITORY https://github.com/pybind/pybind11.git
       GIT_TAG "v${PYBIND11_VERSION}"
       GIT_SHALLOW TRUE
-      PATCH_COMMAND git checkout -- . && git apply --whitespace=fix ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/patches/gil_dec_ref.patch
+      # PATCH_COMMAND git checkout -- . && git apply --whitespace=fix ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/patches/gil_dec_ref.patch
       OPTIONS "PYBIND11_INSTALL ON"
               "PYBIND11_TEST OFF"
   )
